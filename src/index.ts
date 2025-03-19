72cc372d880661801cbeaff7c76045ba36c11fa7
excaoxsal <exca561@gmail.com>
2025-03-19 14:19:56 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,53 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
+import client from './db';
+import cors from '@elysiajs/cors';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(cors());
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+// Endpoint untuk mendapatkan semua folder
+// app.get('/folders', async () => {
+//     const res = await client.query('SELECT * FROM folders');
+//     return res.rows;
+// });
+
+// // Endpoint untuk mendapatkan subfolder dari folder tertentu
+// app.get('/folders/:id/subfolders', async ({ params }) => {
+//     const { id } = params;
+//     const res = await client.query('SELECT * FROM folders WHERE parent_id = $1', [id]);
+//     return res.rows;
+// });
+
+// app.get('/folders/:id/files', async ({ params }) => {
+//   const { id } = params;
+//   const res = await client.query('SELECT * FROM files WHERE folder_id = $1', [id]);
+//   return res.rows;
+// });
+
+app.get('/responded_question', async () => {
+  const res = await client.query('SELECT "DIVISI", COUNT(CASE WHEN "RESPONSE_AUDITEE" IS NOT NULL AND "RESPONSE_AUDITEE" != \'\' THEN 1 END) AS "Open", COUNT(CASE WHEN "RESPONSE_AUDITEE" IS NULL OR "RESPONSE_AUDITEE" = \'\' THEN 1 END) AS "Closed" FROM "RESPONSE_AUDITEE_D" GROUP BY "DIVISI" ORDER BY "DIVISI" ASC;');
+  return res.rows;
+});
+
+app.get('/iso-data', async () => {
+  try {
+      const query = `
+          SELECT 
+              i."NOMOR_ISO", 
+              COUNT(p."ID_MASTER_PERTANYAAN") AS jumlah_pertanyaan
+          FROM "TM_PERTANYAAN" p
+          INNER JOIN "TM_ISO" i ON p."ID_ISO" = i."ID_ISO"
+          GROUP BY i."NOMOR_ISO"
+          ORDER BY i."NOMOR_ISO" ASC;
+      `;
+
+      const res = await client.query(query);
+      return res.rows;
+  } catch (error) {
+      console.error('Error fetching ISO data:', error);
+      return { error: 'Failed to fetch ISO data' };
+  }
+});
+
+app.listen(80);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
